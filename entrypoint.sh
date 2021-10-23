6529208a63cd7b620a555332bc07ea827cd89327
Pierce <piercedhowell@gmail.com>
2021-10-23 15:51:48 -0400

    Adding the Docker files

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source /opt/ros/melodic/setup.bash
+source /catkin_ws/devel/setup.bash
+
+if [ $# -gt 0 ];then
+    # If we passed a command, run it
+    exec "$@"
+else
+    /bin/bash
+fi
+
